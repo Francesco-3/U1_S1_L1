b569007207b31fa3224217a8e07baf41c89d030c
Francesco Dicecca <147667176+Francesco-3@users.noreply.github.com>
2025-09-08 17:35:39 +0200

    update2

--- exercises/src/Ex_3.java
+++ exercises/src/Ex_3.java
@@ -11,20 +11,53 @@
 import java.util.Scanner;
 
 public class Ex_3 {
-    public static int perimetroRettangolo(int a, int b) {
-        return 2 * (a + b);
+    public static int perimetroRettangolo(int base, int altezza) {
+        return 2 * (base + altezza);
+    }
+
+    public static int pariDispari(int n) {
+        return n;
+    }
+
+    public static int perimetroTriangolo(int l1, int l2, int l3) {
+        return l1 + l2 + l3;
     }
 
     public static void main(String[] args) {
+        //perimetroRettangolo
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Inserisci la base: ");
-        int a = scanner.nextInt();
+        int base = scanner.nextInt();
 
         System.out.println("Inserisci l'altezza: ");
-        int b = scanner.nextInt();
+        int altezza = scanner.nextInt();
 
-        System.out.println("Il perimetro del rettangolo è: " + perimetroRettangolo());
+        int perimetro = perimetroRettangolo(base, altezza);
+
+        System.out.println("Il perimetro del rettangolo è: " + perimetro);
+
+        //pariDispari
+        int n = 10;
+
+        if(n % 2 == 0) {
+            System.out.println("0");
+        } else {
+            System.out.println("1");
+        }
+
+        //perimetroTriangolo
+        System.out.println("Inserisci il primo lato: ");
+        int l1 = scanner.nextInt();
+
+        System.out.println("Inserisci il secondo lato: ");
+        int l2 = scanner.nextInt();
+
+        System.out.println("Inserisci il terzo lato: ");
+        int l3 = scanner.nextInt();
+
+        int perimetroT = perimetroTriangolo(l1, l2, l3);
+        System.out.println("Il perimetro del triangolo è: " + perimetroT);
         scanner.close();
     }
 }
